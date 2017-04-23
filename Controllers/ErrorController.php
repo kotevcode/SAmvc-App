9f55b7f6e3b7c90cdeb07ea68bc1034a3f7de512
Shahaf Antwarg <santwarg@gmail.com>
2017-04-24 00:24:09 +0300

    missing error controller

---
+++ Controllers/ErrorController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controllers;
+
+use SAmvc\Framework\Controller;
+
+class ErrorController extends Controller {
+
+  function index()
+  {
+    $this->view->render('header');
+    $this->view->render('errors/index');
+    $this->view->render('footer');
+  }
+
+}
